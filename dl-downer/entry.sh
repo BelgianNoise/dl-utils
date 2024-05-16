b1d4652516b18753b63d6217a52e7d5e9806d42b
Arthur Joppart <arthur.joppart@gmail.com>
2024-05-17 00:50:54 +0200

    chore: use gosu in entry.sh

--- dl-downer/entry.sh
+++ dl-downer/entry.sh
@@ -3,11 +3,10 @@
 PUID=${PUID:-6969}
 PGID=${PGID:-6969}
 
-groupmod -o -g "$PGID" myGroup
-usermod -o -u "$PUID" myUser
+groupadd -g "$PGID" myGroup
+useradd -u "$PUID" -g myGroup myUser
 
 chown -R myUser:myGroup /downloads
-# chown -R myUser:myGroup /cdm
 chown -R myUser:myGroup /storage_states
 
-python start.py
+exec gosu myUser python start.py
