b65a36862532bc134e043a9c489e2e86ddb4d3ab
Arthur Joppart <arthur.joppart@gmail.com>
2024-05-16 23:58:54 +0200

    chore: try docker build in CI cause local is acting up

---
+++ dl-downer/entry.sh
@@ -0,0 +1,15 @@
+#!/usr/bin bash
+
+PUID=${PUID:-6969}
+PGID=${PGID:-6969}
+
+groupmod -o -g "$PGID" myGroup
+usermod -o -u "$PUID" myUser
+
+chown myUser:myGroup /downloads
+chown myUser:myGroup /cdm
+chown myUser:myGroup /storage_states
+
+id
+
+python ./start.py
