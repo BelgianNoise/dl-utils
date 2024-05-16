be98f8b760c031b7781ad069f890b584d4a68865
Arthur Joppart <arthur.joppart@gmail.com>
2024-05-17 00:24:35 +0200

    fix: chown recursively

--- dl-downer/entry.sh
+++ dl-downer/entry.sh
@@ -6,8 +6,6 @@ PGID=${PGID:-6969}
 groupmod -o -g "$PGID" myGroup
 usermod -o -u "$PUID" myUser
 
-chown myUser:myGroup /downloads
-chown myUser:myGroup /cdm
-chown myUser:myGroup /storage_states
-
-id
+chown -R myUser:myGroup /downloads
+# chown -R myUser:myGroup /cdm
+chown -R myUser:myGroup /storage_states
