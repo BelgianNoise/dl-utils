144d5e87efe0073f6b812ea5f9950ce0bf71b57f
Arthur Joppart <arthur.joppart@gmail.com>
2024-05-27 13:33:33 +0200

    Revert "build: [dl-downer] playwright install as user"
    This reverts commit c16154f9aa1ff9f28cb2b8c07b2aeeb794ea2fc6.

--- dl-downer/entry.sh
+++ dl-downer/entry.sh
@@ -27,11 +27,10 @@ fi
 
 # Change ownership of the home directory
 chown -R "$PUID:$PGID" /home/myuser
-chown -R "$PUID:$PGID" /app
 
 chown -R "$PUID:$PGID" /downloads
 chown -R "$PUID:$PGID" /storage_states
 
-gosu $PUID:$PGID playwright install
-# Run the specified command as the new user
-exec gosu $PUID:$PGID "$@"
+# Run the command as the specified user
+set -- gosu "$PUID:$PGID" "$@"
+exec python start.py "$@"
